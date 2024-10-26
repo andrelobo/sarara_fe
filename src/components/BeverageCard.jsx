15abf6cd001d2abe34a957148a62563461046673
André Lobo <loboandre@hotmail.com>
2024-10-26 12:22:46 -0400

    feat: Install SweetAlert2 and refactor Login and Signup components for unified style and error handling

--- src/components/BeverageCard.jsx
+++ src/components/BeverageCard.jsx
@@ -1,40 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const BeverageCard = ({ beverage, onEditBeverage, onDeleteBeverage, onViewHistory }) => {
   if (!beverage) {
     return null;
   }
-
   const { name, category, quantity, unit } = beverage;
-
   return (
-    <div className="bg-gray-800 shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-200">
+    <div className="bg-[#111827] shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-200">
       <div className="bg-gray-900 p-6">
-        <h2 className="text-2xl text-[#f8b431] mb-2">{name}</h2>
+        <h2 className="text-2xl text-yellow-500 mb-2">{name}</h2>
         <p className="text-gray-300 mb-1">
-          <span className="text-[#f8b431]">Categoria:</span> {category}
-        </p>
-
-        <p className="text-gray-300 mb-1">
-          <span className="text-[#f8b431]">Quantidade:</span> {quantity}
+          <span className="text-yellow-500">Categoria:</span> {category}
         </p>
         <p className="text-gray-300 mb-1">
-          <span className=" text-[#f8b431]">Unidade de medida:</span> {unit}
+          <span className="text-yellow-500">Quantidade:</span> {quantity}
+        </p>
+        <p className="text-gray-300 mb-1">
+          <span className="text-yellow-500">Unidade de medida:</span> {unit}
         </p>
         <div className="mt-4 flex justify-between">
-          <button onClick={() => onEditBeverage(beverage)} className="bg-[#c69f56] text-gray-900 px-4 py-2 rounded">
+          <button onClick={() => onEditBeverage(beverage)} className="bg-yellow-600 text-gray-200 px-4 py-2 rounded hover:bg-yellow-700">
             Editar
           </button>
-          <button onClick={() => onDeleteBeverage(beverage._id)} className="bg-red-500 text-gray-900 px-4 py-2 rounded">
+          <button onClick={() => onDeleteBeverage(beverage._id)} className="bg-red-600 text-gray-200 px-4 py-2 rounded hover:bg-red-700">
             Deletar
           </button>
-          <button onClick={() => {console.log('View history clicked for:', beverage._id); onViewHistory(beverage)}} className="bg-blue-500 text-gray-900 px-4 py-2 rounded">
+          <button onClick={() => onViewHistory(beverage)} className="bg-blue-600 text-gray-200 px-4 py-2 rounded hover:bg-blue-700">
             Histórico
           </button>
         </div>
       </div>
     </div>
   );
+};
+
+BeverageCard.propTypes = {
+  beverage: PropTypes.object.isRequired,
+  onEditBeverage: PropTypes.func.isRequired,
+  onDeleteBeverage: PropTypes.func.isRequired,
+  onViewHistory: PropTypes.func.isRequired,
 };
 
 export default BeverageCard;
