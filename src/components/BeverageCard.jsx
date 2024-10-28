49e916c8e9beb07a3835fadc3cdd1944e0135855
André Lobo <loboandre@hotmail.com>
2024-10-28 09:18:56 -0400

    feat:cards new design

--- src/components/BeverageCard.jsx
+++ src/components/BeverageCard.jsx
@@ -1,33 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaEdit, FaTrash, FaHistory } from 'react-icons/fa';
+import Swal from 'sweetalert2';
+import 'sweetalert2/src/sweetalert2.scss';
 
 const BeverageCard = ({ beverage, onEditBeverage, onDeleteBeverage, onViewHistory }) => {
   if (!beverage) {
     return null;
   }
+
   const { name, category, quantity, unit } = beverage;
+
+  // Função para confirmar a exclusão com SweetAlert2
+  const handleDelete = () => {
+    Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Você não poderá desfazer essa ação!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Sim, deletar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onDeleteBeverage(beverage._id);
+        Swal.fire('Deletado!', 'A bebida foi removida.', 'success');
+      }
+    });
+  };
+
   return (
-    <div className="bg-[#111827] shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-200">
-      <div className="bg-gray-900 p-6">
-        <h2 className="text-2xl text-yellow-500 mb-2">{name}</h2>
-        <p className="text-gray-300 mb-1">
-          <span className="text-yellow-500">Categoria:</span> {category}
+    <div className="bg-[#111827] shadow-md rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-200">
+      <div className="bg-gray-900 p-4">
+        <h2 className="text-xl text-blue-500 mb-1">{name}</h2> {/* Alterado para uma cor azul */}
+        <p className="text-gray-400 text-sm mb-1">
+          <span className="font-semibold text-blue-500">Categoria:</span> {category}
         </p>
-        <p className="text-gray-300 mb-1">
-          <span className="text-yellow-500">Quantidade:</span> {quantity}
+        <p className="text-gray-400 text-sm mb-1">
+          <span className="font-semibold text-blue-500">Quantidade:</span> {quantity}
         </p>
-        <p className="text-gray-300 mb-1">
-          <span className="text-yellow-500">Unidade de medida:</span> {unit}
+        <p className="text-gray-400 text-sm mb-1">
+          <span className="font-semibold text-blue-500">Unidade de medida:</span> {unit}
         </p>
-        <div className="mt-4 flex justify-between">
-          <button onClick={() => onEditBeverage(beverage)} className="bg-yellow-600 text-gray-200 px-4 py-2 rounded hover:bg-yellow-700">
-            Editar
+
+        <div className="mt-4 flex justify-around text-gray-200">
+          <button onClick={() => onEditBeverage(beverage)} className="hover:text-yellow-400">
+            <FaEdit size={20} />
           </button>
-          <button onClick={() => onDeleteBeverage(beverage._id)} className="bg-red-600 text-gray-200 px-4 py-2 rounded hover:bg-red-700">
-            Deletar
+          <button onClick={handleDelete} className="text-red-500 hover:text-red-600">
+            <FaTrash size={20} />
           </button>
-          <button onClick={() => onViewHistory(beverage)} className="bg-blue-600 text-gray-200 px-4 py-2 rounded hover:bg-blue-700">
-            Histórico
+          <button onClick={() => onViewHistory(beverage)} className="hover:text-blue-400">
+            <FaHistory size={20} />
           </button>
         </div>
       </div>
--- src/pages/BeverageHistoryPage.jsx
+++ src/pages/BeverageHistoryPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import BeverageHistoryChart from "../components/BeverageHistoryChart";
 
-const BeverageHistoryPage = () => {
-  return (
-    <div>
-     
-      <BeverageHistoryChart />
-    </div>
-  );
-};
+const BeverageHistoryPage = () => (
+  <div className="page-container">
+    <BeverageHistoryChart />
+  </div>
+);
 
 export default BeverageHistoryPage;
+
